40077bf5fbda612f8830ed120d61d6156ccddf55
guymacin <guyskv@gmail.com>
2022-11-21 20:22:02 +0200

    Application class

---
+++ app/src/main/java/com/guy/class23a_ands_3/MyApp.java
@@ -0,0 +1,13 @@
+package com.guy.class23a_ands_3;
+
+import android.app.Application;
+
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
